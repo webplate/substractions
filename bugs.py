f3eea971fb6e6cb08169c2d7a850b85c2893a201
webplate <webplate@gmail.com>
2013-05-09 22:20:42 +0200

    detect incomplete subs

--- bugs.py
+++ bugs.py
@@ -61,17 +61,39 @@ def bugId(n1, n2, result):
     '''
     bugs = []
     correct = str(int(n1) - int(n2))
-    if clean_rslt(result) != correct :     #look for bugs only if erroneous result
+    cresult = clean_rslt(result)
+    if cresult != correct :     #look for bugs only if erroneous result
         max_col = max(len(n1),len(n2),len(result))  #how many colons in the subtraction ?
         min_col = min(len(n1),len(n2),len(result))
         result = completeX(max_col, result)
-        for i in range(max_col):
+        i = 0
+        while i < max_col :
             pos = -i-1              #explore numbers from right to left (align to right)
             if i < len(n1):            #if result is not longer than operands
                 d1 = int(n1[pos])
                 if i < min_col :            #focus on completed columns
                     d2 = int(n2[pos])
-                    bugs.append(bugId_perDigit(d1, d2, result[pos]))
+                    #check for incomplete subs : look only for erroneous
+                    #columns if they're not from incomplete mental subtraction
+                    #for this consider also digit from next column
+                    correct2 = correct[pos-1:len(correct)+pos+1]
+                    n1_2 = n1[pos-1:len(n1)+pos+1]
+                    n2_2 = n2[pos-1:len(n2)+pos+1]
+                    result2 = cresult[pos-1:len(cresult)+pos+1]
+                    try :
+                        r2 = int(result2)
+                    except ValueError :
+                        check_incomplete = False
+                    else :
+                        check_incomplete = (r2 == int(n1_2) - int(n2_2)
+                            and r2 - int(n1_2) <= mental_limit)
+                    if check_incomplete :
+                        bugs.append(['incomplete'])
+                        #and skip next column
+                        bugs.append(['incomplete'])
+                        i += 1
+                    else :
+                        bugs.append(bugId_perDigit(d1, d2, result[pos]))
                 else :        #then search for 'blank' bugs
                     d2 = int(n2[-min_col])
                     bug = bugId_perDigit(d1, d2, result[pos])
@@ -81,6 +103,8 @@ def bugId(n1, n2, result):
                         bugs.append(bug)
             else :
                 bugs.append(['over'])           #subject has written too many digits
+            #process next column
+            i += 1
     else :
         bugs.append(['correct'])        #subject is correct (operation level)
     #~ print n1,n2,result,bugs
@@ -133,27 +157,35 @@ def find_dominant(found, possible):
     NOT YET !!
     '''
     score = 0
-    for i, bugs in enumerate(found) :
-        for b in possible[i] :
-            if b in bugs :
-                print b
+    for i, op_bugs in enumerate(found) :
+        for col_bug in possible[i] :
+            if col_bug in op_bugs :
+                print op_bugs, col_bug
                 score += 1
+    print score
     return score
+
+def dominancy(found, possible):
+    '''Returns a dominancy score for found bugs
+    '''
+    pass
 
 #~ count_correct(data, ref)
 #~ print len(data)
 
 #~ print bugId_perDigit(9,2,7)
+#~ 
+print bugId('1813','215','1598'), 'correct'
+print bugId('1813','215','1600'), 'pt-gd=0'
+print bugId('1813','215','1700'), 'unexplained'
+print bugId('1813','215','170X')
+print bugId('1813','215','070X'), 'test not full col'
+print bugId('647', '45', '706')
+print bugId('1813','215','11598'), 'over'
+print bugId('1813','215','001598'), 'zero on left'
+print bugId('562','3','259'), 'incomplete sub : should only see blank bug as 62 - 3 = 59'
+print bugId('562','24','542'), 'incomplete sub (56-2=54)'
 
-#~ print bugId('1813','215','1598'), 'correct'
-#~ print bugId('1813','215','1600'), 'pt-gd=0'
-#~ print bugId('1813','215','1700'), 'unexplained'
-#~ print bugId('1813','215','170X')
-#~ print bugId('1813','215','070X'), 'test not full col'
-#~ print bugId('647', '45', '706')
-#~ print bugId('1813','215','11598'), 'over'
-#~ print bugId('1813','215','001598'), 'zero on left'
-print bugId('562','3','259'), 'should only see blank bug as 62 - 3 = 59'
 
 #~ for subject in data:
     #~ for i, result in enumerate(subject['results']):
@@ -165,6 +197,8 @@ print bugId('562','3','259'), 'should only see blank bug as 62 - 3 = 59'
 
 #~ print possible_bugs('83','44')
 #~ out= possible_sheet([('647','45'),('885','205')])
+
+#~ out = possible_sheet(operations)
 
 #~ found = subject_sheet_bugs(data[0]['results'], operations)
 #~ dom = find_dominant(found, poss_sheet)
