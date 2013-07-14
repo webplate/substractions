fe970e09545a34ffec7ad48328c2951f420c4a76
webplate <webplate@gmail.com>
2013-07-14 19:53:01 +0200

    got new feats from master

--- bugs.py
+++ bugs.py
@@ -244,14 +244,10 @@ def simulate(dom_bugs, poss_sheet) :
     simul = [ [] for i in range(len(poss_sheet)) ]
     results = []
     for index, subtraction in enumerate(poss_sheet) :
-        #~ print subtraction
         #select possible productions for a sub
         for bug in subtraction :
             #~ positions = [ bug['pos'] for bug in simul[index] ]
             gen_pos = [ bugg['pos'] for bugg in simul[index] ]
-            print simul[index]
-            #~ for bugo in simul[index] :
-                #~ print bugo['pos']
             if (bug['type'] in dom_bugs
             or (bug['type'] in ('correct_col', 'copy')
             and bug['pos'] not in gen_pos)) :
@@ -260,9 +256,7 @@ def simulate(dom_bugs, poss_sheet) :
         #build the corresponding result
         positions = [ bug['pos'] for bug in simul[index] ]
         result_lst = [ '?' for i in range(-min(positions)) ]
-        #~ print result_lst, simul[index]
         for bug in simul[index] :
-            #~ print bug['pos'], bug['result']
             result_lst[bug['pos']] = bug['result']
         result = ''
         for c in result_lst :
@@ -300,14 +294,14 @@ def simulate(dom_bugs, poss_sheet) :
 
 
 #~ pprint.pprint(possible_bugs('647','45'))
-data, ref, operations = r_d.load_data(parameters.dataPath,parameters.subject_pattern,parameters.reference,parameters.subtractions)
-
-found = subject_sheet_bugs(data[parameters.default_sub]['results'], operations)
+#~ data, ref, operations = r_d.load_data(parameters.dataPath,parameters.subject_pattern,parameters.reference,parameters.subtractions)
+#~ 
+#~ found = subject_sheet_bugs(data[parameters.default_sub]['results'], operations)
 #~ pprint.pprint(operations)
 #compute dominancies of subject
-scores = dominancy(found, poss_sheet)
+#~ scores = dominancy(found, poss_sheet)
 #create profile of subject (most dominant bugs)
-dom_bugs = profile(scores, parameters.dominancy_thre)
+#~ dom_bugs = profile(scores, parameters.dominancy_thre)
 #compute simulation according to profile
-simul_sheet = simulate(dom_bugs, poss_sheet)
-pprint.pprint(simul_sheet[1])
+#~ simul_sheet = simulate(dom_bugs, poss_sheet)
+#~ pprint.pprint(simul_sheet[1])
